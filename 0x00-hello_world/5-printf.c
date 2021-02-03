bb9575c459a3f101b0cda1eff38c3d0b3262d511
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-03 23:36:19 +0000

    Pushing file to step 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point, will print a string to stdout
+ *
+ * Return: Always 0
+ */
+int main {void}
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
