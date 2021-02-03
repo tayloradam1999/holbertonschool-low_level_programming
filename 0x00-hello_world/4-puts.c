973825b7d5c8ce694d39955c5f9b9c9c9ea917ae
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-03 23:05:49 +0000

    Edited file for step 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc puts(Programming is like building a multilingual puzzle\n);
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a muiltilingual puzzle\n");
+	return 0;
+}
