53f5574f37b0021a343560d1a2608b8a10062876
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-03 23:17:18 +0000

    Edited file for step 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - Entry point, will print a string to stdout
+ * Return: Prints 0 if successful
+ */
 int main(void)
 {
 puts("\"Programming is like building a muiltilingual puzzle");
