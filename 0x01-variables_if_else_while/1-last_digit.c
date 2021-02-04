2f1325e65dd7cd50718ca59a16a3af59c1b2d35a
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-04 23:20:36 +0000

    Editing file for step 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - Assigns a random number to the var "n" each time
  * it is executed.
- * main will print the last digit of the number stored in the var "n"
+ * Will print the last digit of the number stored in the var "n"
  *
  * Return: 0!
  */
@@ -19,9 +19,9 @@ int main(void)
 	
 	if (last > 5);
 	printf("The last digit of %d is %d and is greater than 5\n", n, last);
-	else if (last == 0);
+	if (last == 0);
 	printf("The last digit of %d is %d and is 0\n", n, last);
-	else (last < 6) && (last != 0);
+	if (last < 6) && (last != 0);
 	printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last);
 
 	return (0);
