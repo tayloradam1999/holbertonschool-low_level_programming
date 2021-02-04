f45152fda8876fa305e4b9c77fee825a7a788f89
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-04 02:12:02 +0000

    Edited file for step 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Entry point, will print sizes of various types on the computer it is compiled and run on.
- *
  * Return: Always 0
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -13,6 +10,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 	return (0);
 }
