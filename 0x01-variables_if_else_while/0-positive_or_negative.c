8d494b895d722bb103e62a6e9864f5fee058febd
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-04 22:53:26 +0000

    Edited file for step 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,16 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
+
 	else if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
+
 	else
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }    
