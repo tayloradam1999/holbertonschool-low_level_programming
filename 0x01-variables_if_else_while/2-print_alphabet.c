130fe8d65e8cb0020d54a70a23b8936b4b12d2fe
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-04 23:32:38 +0000

    Pushing file to step 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed be a new line
+ *
+ * Return: 0!
+ *
+ */
+
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+		x = tolower(x);
+		putchar(x);
+	{
+	return 0;
+}
+
