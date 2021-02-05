2ec69162eef155c51273e8c898356015a4355d76
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 02:29:37 +0000

    Pushing file to step 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.>
+
+/**
+ * main - prints out all possible combos of single
+ * digit numbers
+ *
+ * Return: 0 baby! surprise!
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; number <= 9; number++)
+	{
+		putchar(digit + '0');
+
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+putchar('\n');
+return (0);
+}
