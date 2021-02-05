abbe302169e2469cf66909d32837e254cccaaa83
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 00:41:33 +0000

    Edited file for step 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	char = alphabet;
+	char alphabet;
 
 	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 	{
 		if (alphabet != 'q' && alphabet != 'e')
-		putchar(alphabet);
+			putchar(alphabet);
 	}
 putchar('\n');
 return (0);
