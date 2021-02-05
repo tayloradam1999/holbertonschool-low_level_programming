2e2156d4bd4e13fb59b5d60d30da04d1970857dd
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 00:37:33 +0000

    Pushing file to step 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ * Prints all letters except 'q' and 'e'
+ *
+ * Return: 0!
+ */
+
+int main(void)
+{
+	char = alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		putchar(alphabet);
+	}
+putchar('\n');
+return (0);
+}
