126808091acc2ca70f2f1b3134d53a859f6478e8
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 02:16:41 +0000

    Pushing file to step 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 in lowercase,
+ * followed by a new line
+ *
+ * Return: 0!!!!
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = 0; digit >=  9; digit++)
+		putchar(digit);
+
+	for (digit = a; digit >= 'f'; digit++)
+		putchar(digit);
+
+	putchar('\n');
+	return (0);
+}
