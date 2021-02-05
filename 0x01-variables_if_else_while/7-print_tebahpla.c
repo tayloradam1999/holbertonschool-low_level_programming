00f1f2f3ee861bff0140fc148c07b8d8402ddcf8
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 02:08:58 +0000

    Edited file for step 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,8 @@ int main(void)
 {
 	char rev_alpha;
 
-	for (rev_alpha = 'a' ; rev_alpha < 'z' ; rev_alpha++)
-		gets(rev_alpha);
-
-		strrev(rev_alpha);
-
-	putchar(rev_alpha);
+	for (rev_alpha = 'z' ; rev_alpha >= 'a' ; rev_alpha++)
+		putchar(rev_alpha);
 
 	putchar('\n');
 	return (0);
