cf0f5ce7859125e26a580537ed6873338774565c
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 00:12:25 +0000

    Edited file for step 3

--- 0x01-variables_if_else_while/3-print_alphabetcs.c
+++ 0x01-variables_if_else_while/3-print_alphabetcs.c
@@ -9,16 +9,18 @@
 
 int main(void)
 {
-char alphabet;
+	char alphabet;
 
-for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
-{
-putchar(alphabet);
-}
-for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
-{
-putchar(alphabet);
-}
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
 putchar('\n');
 return (0);
 }
