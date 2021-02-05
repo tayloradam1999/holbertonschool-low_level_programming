32239a1fab0dec39951caa65b28e9d074636c2b6
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 02:22:11 +0000

    Edited file for step 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	char digit;
 
-	for (digit = '0'; digit >= '9'; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
 
-	for (digit = 'a'; digit >= 'f'; digit++)
+	for (digit = 'a'; digit <= 'f'; digit++)
 		putchar(digit);
 
 	putchar('\n');
