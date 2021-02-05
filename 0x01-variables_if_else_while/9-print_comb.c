8ac02eb775fa9d94158ee42b27fa5c4dc6e4b9dc
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-05 04:21:27 +0000

    Pushing file to step 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,15 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
-	{
+
 		putchar(digit + '0');
 
 		if (number != 9)
-		{
+
 			putchar(',');
 			putchar(' ');
-		}
-	}
+
+
 putchar('\n');
 return (0);
 }
