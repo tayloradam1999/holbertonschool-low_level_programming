58ab6fdd4250ccf2f003136ad9735998388f15e5
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 15:42:07 +0000

    Edited file for task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _putchar - Prints Holberton followed by a new line
+ * main - Prints Holberton followed by a new line
  *
  * Return: No return
  */
@@ -11,11 +11,9 @@ int main(void)
 	char t[10] = "Holberton";
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 8; a++)
 		_putchar(t[a]);
 
 	_putchar('\n');
 	return (0);
 }
-
-
