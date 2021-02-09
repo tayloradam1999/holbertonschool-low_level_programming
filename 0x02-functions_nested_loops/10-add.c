e0662b33a44c011cb4517f624e1df486edf59a1a
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 04:38:36 +0000

    Pushing file to step 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * @x: x + y
+ * @y: x + y
+ * main - adds two integers and returns the result
+ * Return: value of x + y
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
