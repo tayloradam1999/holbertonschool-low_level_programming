4904402ac942c5db24b25e46b309abaed96818f1
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 16:52:47 +0000

    Edited comments

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 
 /**
  * _abs - Computes the absolute value of an int
- * @x: generates absolute value
+ * @r: some number 
  * Return: absolute value
  */
 
-int _abs(int x)
+int _abs(int r)
 {
 	if (r > 0)
 	{
