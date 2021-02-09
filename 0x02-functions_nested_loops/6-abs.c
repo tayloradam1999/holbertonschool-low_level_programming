7e823804604cca4c0276d3b04d83bcdcda639622
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 00:17:40 +0000

    Pushing file to step 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _abs - Computes the absolute value of an int
+ * @x: generates absolute value
+ * Return: absolute value
+ */
+
+int _abs(int x)
+{
+	int abs(int x);
+	return (x);
+}
