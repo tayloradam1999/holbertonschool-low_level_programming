ee511dadb25c080ccdd4618f4e95e5bcd6a24ee5
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 20:12:16 +0000

    Edited file for task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Return: null
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d = '0';
+
+	while (d <= '2')
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			for (b= '0'; b <= '5'; b++)
+			{
+				for (a = '0'; a <= '9'; a++)
+				{
+					if ((d == '2') && (c > '3'))
+					{
+						break;
+					}
+					_putchar(d);
+					_putchar(c);
+					_putchar(':');
+					_putchar(b);
+					_putchar(a);
+					_putchar('\n');
+				}
+			}
+		}
+	d++;
+	}
+}
