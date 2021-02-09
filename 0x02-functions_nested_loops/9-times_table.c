8f13134e6855f06fda6e3e9f4ded13008293bd3d
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 21:08:24 +0000

    Pushing file to task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, x, y;
+	int z = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			a = z / 10;
+			b = z % 10;
+			if (z > 9)
+			{
+				_putchar(a + '0');
+			}
+			if (z < 10)
+			{
+				_putchar(' ');
+			}
+			_putchar(b + '0');
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			z = z + x;
+		}
+	_putchar('\n');
+	z = 0;
+	}
+}
