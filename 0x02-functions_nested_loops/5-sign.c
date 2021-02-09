25bba577df6927eb8ebad219baaccc7cf0f80c7d
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 16:42:22 +0000

    Edited file for task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,14 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	if (n > 0)
+	else if (n > 0)
 	{
 		_putchar('+');
-		return (1);	
+		return (1);
 	}
-	if (n == 0)
+	else
 	{
 		_putchar('0');
 		return (0);
 	}
-return (9);
 }
