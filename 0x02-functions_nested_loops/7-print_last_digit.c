83a2d51ebb630ebb14dc5e067a6c3a85588a60eb
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-09 17:23:02 +0000

    Edited file for task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,14 @@ int print_last_digit(int x)
 	int y;
 
 	if (x >= 0)
+	{
 		y = x % 10;
-
+		_putchar(y + '0');
+	}
 	else
+	{
 		y = -x % 10;
-
-	_putchar (y + '0');
+		_putchar(y + '0');
+	}
 	return (y);
 }
