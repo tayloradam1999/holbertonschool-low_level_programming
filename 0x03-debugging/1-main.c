3a34b5ab85e50d355072d65435eb2a57777201bd
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-10 01:49:03 +0000

    Edited file for task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
+        /* while (i < 10)
         {
-                putchar(i); /* infinite loop starts here */
+                putchar(i); 
         }
-
+	*/
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
