7be572e0e8299f94b6bdc3c261f3289cd4b66ca6
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-11 17:17:07 +0000

    Pushing file to task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 excluding 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != '2' && x != '4')
+		{
+			_putchar(x);
+		}
+	}
+	
+	_putchar('\n');
+}
