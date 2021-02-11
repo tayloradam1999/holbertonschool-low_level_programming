11f62c4f011e03852da7357e3c5b36b39e1ab37d
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-11 21:48:15 +0000

    Pushing file to task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - Prints a square with a new line
+ * @size; Size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
