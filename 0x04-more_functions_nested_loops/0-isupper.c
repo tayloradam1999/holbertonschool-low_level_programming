e938faaeb4ca3cceb9c9cca909a03414e656b480
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-11 15:59:54 +0000

    Pushing file to task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Checks for uppercase characters
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z');
+	{
+		return (1);
+	}
+	return (0);
+}
