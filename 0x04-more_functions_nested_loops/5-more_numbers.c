3c1f30f2dad0959d9b420e8bab2cafa9c44e4085
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-11 20:27:08 +0000

    Pushing file to task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14
+ * followed by a new line
+ * Return : void
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+			_putchar('0' + y / 10);
+			}
+			_putchar('0' + y % 10);
+		}
+	_putchar('\n');
+	}
+}
