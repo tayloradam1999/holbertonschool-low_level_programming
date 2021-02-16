41632e389fb55844be7eb08fc1a18e550e2b187c
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-16 17:10:08 +0000

    Pushing file for task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the value of two ints
+ * @a: First int
+ * @b: Second int
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
