a4d758f423b6f579d8302ab165088188226b08ba
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-16 19:45:08 +0000

    Taking out if loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,10 @@ void puts2(char *str)
 {
 	int x = 0;
 
-	while (*(str + x))
+	while (*(str + x) != '\0')
 	{
-		if (*(str + x) != '\0')
-		{
 		_putchar(*(str + x));
 		x = x + 2;
-		}
 	}
 	_putchar('\n');
 }
