2ed69ec7c69627205c3f36c8db36e5d5d4a9a3e1
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-18 23:41:41 +0000

    Pushing file for task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Changes lowercase letters to uppercase
+ * @str: The string to manipulate
+ * Return: The string in all uppercase
+ */
+
+char *string_toupper(char *str);
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] >= 'a' && str[x] <= 'z')
+		{
+			str[x] -= 32;
+		}
+
+	return (str);
+}
