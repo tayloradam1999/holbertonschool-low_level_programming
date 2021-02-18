4e186eea191edac70abb63b23151be37251f3f2a
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-18 20:27:26 +0000

    Pushing file to task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,4 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
