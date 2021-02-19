299f6c1086fb097e128b4b2a102c9f2429b3d8db
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-19 01:41:34 +0000

    Pushing file for step 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @x: Pointer
+ * Return: The encoded string
+ */
+
+char *rot13(char *x)
+{
+	int y = 0;
+	char letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char number[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int yy;
+
+	for (; x[y] != '\0'; y++)
+	{
+		for (yy = 0; letter[yy] != '\0'; yy++)
+		{
+			if (x[y] == letter[yy])
+			{
+				x[y] = number[yy];
+			}
+		}
+	}
+	return (x);
+}
