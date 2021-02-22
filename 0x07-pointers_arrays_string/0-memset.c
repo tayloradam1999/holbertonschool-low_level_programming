03f8a5713103bf40e6d06ba592d812ed0eb3c3e5
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-22 22:32:39 +0000

    Pushing directory, README, and header file

---
+++ 0x07-pointers_arrays_string/0-memset.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: Pointing to memory
+ * @b: Constant byte
+ * @n: How many bytes
+ * Return: Pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
