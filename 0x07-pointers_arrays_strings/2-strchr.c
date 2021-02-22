3c2965efdb47cc380eacb079f897b94bf5f7a962
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-22 23:05:42 +0000

    Pushing file for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a char in a str
+ * @s: The string
+ * @c: The char
+ * Return: The character found or NULL if no char is found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+		if (s[x] == c)
+		{
+			break;
+		}
+	}
+
+	return (s);
+}
