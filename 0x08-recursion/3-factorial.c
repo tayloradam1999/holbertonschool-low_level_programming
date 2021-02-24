1acafad07c6706ccc4329814846e10172fa6ba89
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-24 22:21:03 +0000

    Pushing file for task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * factorial - Returns the factorial of a given number
+ * @n: Given number
+ * Return: Factorial
+ */
+
+int factorial(int n)
+{
+	if (n >= 1)
+	{
+		return (n*factorial(n - 1));
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n);
+}
