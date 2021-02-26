f8ba3a6b6b00d834cf3625ea267344d50144154a
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-26 19:32:10 +0000

    Pushing file for task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints all arguments it receives
+ * @argc: Number of commands passed
+ * @argv: Array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int counter;
+
+	for (counter = 0; counter < argc; counter++)
+	{
+		printf("%s\n", argv[counter]);
+	}
+	return (0);
+}
