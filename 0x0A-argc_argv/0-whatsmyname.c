6ac673bf16f502de93eb6ea2fa12db5fe9eb8af3
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-02-26 18:56:23 +0000

    Pushing file for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints the program name followed by a new line.
+ * @argc: Number of commands in terminal
+ * @argv: Array of strings
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+return (0);
+}
