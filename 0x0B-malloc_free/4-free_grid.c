1471b0d5b8e1d6b5a1a9ac24663a7cc2e4547d8c
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-03 04:30:51 +0000

    Pushing file for task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**                                                                                             
+ * free_grid - free 2d grid                                                                     
+ * @grid: pointer to pointer                                                                    
+ * @height: int                                                                                 
+ * Return: void                                                                                 
+ *                                                                                              
+ */
+void free_grid(int **grid, int height)
+{
+        (void)height;
+        free(grid);
+}
