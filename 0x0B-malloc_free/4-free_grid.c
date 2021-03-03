522f5dc610698ac672fc2371bd5b34d479f05841
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-03 20:54:41 +0000

    Made Betty a happy lady.

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-/**                                                                                             
- * free_grid - free 2d grid                                                                     
- * @grid: pointer to pointer                                                                    
- * @height: int                                                                                 
- * Return: void                                                                                 
- *                                                                                              
+
+/**
+ * free_grid - Frees a 2 dimensional array
+ * @grid: Grid to free
+ * @height: height of grid
+ * Return: void
  */
+
 void free_grid(int **grid, int height)
 {
-        (void)height;
-        free(grid);
+	(void)height;
+	free(grid);
 }
