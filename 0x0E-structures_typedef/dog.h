934a6957fa405f912705a2432234ded05aad345c
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-09 05:09:34 +0000

    Fixed up betty errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,19 @@
 #define DOG
 
 /**
- * strcuct dog - Dog struct
+ * struct dog - Dog struct
  * @name: Name of dog
  * @age: Age of dog
  * @owner: Owner of dog
  */
 
-tyepdef struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
