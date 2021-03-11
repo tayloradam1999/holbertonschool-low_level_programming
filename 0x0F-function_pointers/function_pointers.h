6baf3068b1fef815425364d072027afa95409509
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-11 05:02:38 +0000

    Added include stdio

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -9,4 +10,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
