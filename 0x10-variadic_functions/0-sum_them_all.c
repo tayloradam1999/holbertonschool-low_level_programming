13239d473e8b85089e4499bbf97ac38fa7a90689
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-11 21:27:15 +0000

    Fixed betty error, added if statement that checks for n == 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int count;
 	int sum = 0;
-
 	va_list list;
+
 	va_start(list, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	for (count = 0; count <= n; count++)
 	{
 		sum += va_arg(list, int);
