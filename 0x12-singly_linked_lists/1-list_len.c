f46bd494b3b19ff3d9c5b206e29e539c27f978ec
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-18 20:17:04 +0000

    Pushing file for task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns length of list
+ * @h: Pointer to head
+ * Return: Length of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int x = 0;
+
+	while(h)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
