14c6549ac64b1c8c7361e45b50ab1cffd18b28d4
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-18 19:59:36 +0000

    Pushing file for task 0 and added struct

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_list - Prints all elements in a singly linked list
+ * @h: Pointer to head
+ * Return: Number of total nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int x = 1;
+
+	if (h == NULL)
+		return (0);
+
+	for (; h->next != NULL; x++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h-> len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (x);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,12 @@
 #ifndef SINGLY_LISTS
 #define SINGLY_LISTS
 
+typedef struct sllist {
+	char *data;
+	unsigned int len;
+	struct sllist *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
