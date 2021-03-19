8d5cddd69f198af89cae8e5be553d0631093c62e
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-19 19:32:00 +0000

    Pushing file for task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * premain - Compilation before main function
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're Beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
