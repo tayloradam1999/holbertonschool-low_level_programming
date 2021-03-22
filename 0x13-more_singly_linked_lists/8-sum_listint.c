f06926f0ba36265df7ea7e2e0126078701810e1d
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-22 21:03:35 +0000

    Pushing file for task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Returns the sum of all the data of a linekd list
+ * @head: Pointer to head
+ * Return: Sum of all data in list
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *new_node;
+	int sum;
+
+	new_node = head;
+	while (new_node)
+	{
+		sum += new_node->n;
+		new_node = new_node->next;
+	}
+	return (sum);
+}
