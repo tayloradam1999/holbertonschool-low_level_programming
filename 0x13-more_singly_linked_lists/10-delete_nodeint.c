11f296907b2f3cfd4c588a70c6e9bcfb0f45603b
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-23 21:04:42 +0000

    Pushing file for task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes the node at a given index
+ * @head: Pointer to head
+ * @index: Where to delete node
+ * Return: 1 if successful, -1 if failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *new_node, *trav;
+	unsigned int x = 0, y = 0;
+
+	trav = *head;
+	while (trav)
+	{
+		y++;
+		trav = trav->next;
+	}
+	if (index >= y)
+		return (-1);
+	trav = *head;
+	if (index == 0)
+	{
+		*head = trav->next;
+		free(trav);
+	}
+	else
+	{
+		for (x = 0; trav != NULL && x < (index - 1); x++)
+			trav = trav->next;
+		if (trav == NULL || trav->next == NULL)
+			return (-1);
+		new_node = trav->next->next;
+		free(trav->next);
+		trav->next = new_node;
+	}
+	return (-1);
+}
