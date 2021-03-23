7039d43ee6b63b33bea72369057502474b9a0201
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-23 18:04:42 +0000

    Fixed betty comments and took out ->next in while loop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - Frees a list
- * @h: Pointer to head
+ * @head: Pointer to head
  * Return: void
  */
 
@@ -10,13 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *new_node;
 
-	while ((*head)->next != NULL)
+	while ((*head) != NULL)
 	{
 		new_node = (*head)->next;
 		free(*head);
 		*head = new_node;
 	}
 
-	free(*head);
 	*head = NULL;
 }
