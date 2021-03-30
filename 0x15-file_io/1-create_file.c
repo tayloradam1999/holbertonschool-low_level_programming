abbb433bdb57f5cdc30f1aa2dbf8190326f2646e
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-30 20:26:19 +0000

    Pushing file for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file to create and text_content is a NULL
+ * terminated string to write to the file
+ * @text_content: What is going inside the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t write_Check;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	write_Check = write(fd, text_content, _strlen(text_content));
+	if (write_Check == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - Finds length of a string
+ * @s: String
+ * Return: Length
+ */
+ssize_t _strlen(char *s)
+{
+	ssize_t x = 0;
+
+	for (; s[x]; x++)
+		continue;
+	return (x);
+}
