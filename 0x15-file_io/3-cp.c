84c5bc9ea49cb473f9b728f457152d0d5cfd40ef
Adam Taylor <tayloradam1999@gmail.comgit config --global user.name Adam>
2021-03-31 18:58:55 +0000

    Replaced a comma for a colon on close edge case

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	if (close(inputFd) == -1)
-	{dprintf(STDERR_FILENO, "Error, Can't close fd %s\n", argv[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
 		exit(100); }
 	if (close(outputFd) == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
