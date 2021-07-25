51d9b59f8b34b7b57eade5c04a4fef8a14a81a9c
Adam Taylor <tayloradam1999@gmail.com>
2021-07-25 22:08:01 +0000

    Fixed all betty indent errors

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,12 +1,18 @@
+/**
+ * hash_djb2 - Algorithim for Hash Tables
+ * @str: String to be passed to convert to binary
+ * Return: New Hash Table from input string
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
