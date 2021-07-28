10b3342fd356026871ca8a33a8833237a3905b40
Adam Taylor <tayloradam1999@gmail.com>
2021-07-28 21:45:21 +0000

    Pushing new format found from github resource

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,6 +1,30 @@
 #include "hash_tables.h"
 
 /**
+ * ht_newpair - Creates new key-value pair
+ * @key: Key to new pair
+ * @value: Value to new pair
+ * Return: New pair
+ */
+
+hash_node_t *ht_newpair(const char *key, const char *value)
+{
+	hash_node_t *newpair;
+
+	newpair = malloc(sizeof(hash_node_t));
+	if (newpair == NULL)
+		return (NULL);
+	newpair->key = strdup(key);
+	if (newpair->key == NULL)
+		return (NULL);
+	newpair->value = strdup(value);
+	if (newpair->value == NULL)
+		return (NULL);
+	newpair->next = NULL;
+	return (newpair);
+}
+
+/**
  * hash_table_set - Adds element to Hash Table
  * @ht: Hash Table to use
  * @key: Key as to where to insert element
@@ -10,36 +34,42 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx;
-	hash_node_t *out;
+	unsigned long int idx = 0;
+	hash_node_t *newpair = NULL;
+	hash_node_t *next = NULL;
+	hash_node_t *last = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL)
 		return (0);
-
-	if (key[0] == '\0')
-		return (0);
-
-	out = malloc(sizeof(hash_node_t));
-	if (out == NULL)
-		return (0);
-
-	out->key = strdup(key);
-	if (out->key == NULL)
-	{
-		free(out);
-		return (0);
-	}
-
-	out->value = strdup(value);
-	if (out->value == NULL)
-	{
-		free(out->key);
-		free(out);
-		return (0);
-	}
 
 	idx = key_index((const unsigned char *)key, ht->size);
-	out->next = ht->array[idx];
-	ht->array[idx] = out;
+	next = ht->array[idx];
+
+	if (next != NULL && next->key != NULL && strcmp(key, next->key) > 0)
+	{
+		free(next->value);
+		next->value = strdup(value);
+	}
+	else
+	{
+		if (next == ht->array[idx])
+		{
+			newpair = ht_newpair(key, value);
+			if (!newpair)
+				return (0);
+			if (next == ht->array[idx])
+			{
+				newpair->next = next;
+				ht->array[idx] = newpair;
+			}
+			else if (next == NULL)
+				last->next = newpair;
+			else
+			{
+				newpair->next = next;
+				last->next = newpair;
+			}
+		}
+	}
 	return (1);
 }
